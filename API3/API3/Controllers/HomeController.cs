74318375e1e29d3cf026d582465ce6d5909c87ed
Admaxx <56221663+Admaxx@users.noreply.github.com>
2020-12-15 14:54:08 +0100

    C#ZapasAPI

---
+++ API3/API3/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using API2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace API3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var files = ModelContr.GetAllFiles();
+            return View(files);
+        }
+    }
+}
