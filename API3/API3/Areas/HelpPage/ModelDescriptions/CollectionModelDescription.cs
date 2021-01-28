b635edcd0c97a5f4a17953afe5428e71d8566f78
Arczi-pl <artur@DESKTOP-RV2FIAB.localdomain>
2021-01-28 11:25:03 +0100

    porzadki w repo

--- API3/API3/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace API3.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
