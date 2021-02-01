0121a30b27a07c8e854447de2b44ec1507fd6706
Arczi-pl <56255448+Arczi-pl@users.noreply.github.com>
2021-02-01 23:06:43 +0100

    Zabezpieczenie sesji

--- React/src/components/Main.js
+++ React/src/components/Main.js
@@ -15,22 +15,28 @@ import {
 } from 'react-router-dom';
 
 class Main extends React.Component {
+
   render() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/main'><Redirect to='/transfer' /></Route>
-          <Route path='/account' component={Account} />
-          <Route path='/links' component={Links} />
-          <Route path='/transfer' component={Transfer} />
-          <Route path='/about' component={About} />
-          <Route path='/logout' render={() => {logout()} } />
-        </Switch>
-      </Router>
-  </>
-  );
+  if(inSession()){ // dodatkowe zabepieczenie 
+      return (
+        <>
+          <Router>
+            <Navbar />
+            <Switch>
+              <Route path='/main'><Redirect to='/transfer' /></Route>
+              <Route path='/account' component={Account} />
+              <Route path='/links' component={Links} />
+              <Route path='/transfer' component={Transfer} />
+              <Route path='/about' component={About} />
+              <Route path='/logout' render={() => {logout()} } />
+            </Switch>
+          </Router>
+      </>
+      );
+    }
+  else {
+    return (<> </>);
+  }
   }
 }
 
@@ -40,6 +46,31 @@ function logout(){
     cookies.set("refresh_token", "None");
     let path = "/"; 
     window.location.href=path;
+}
+
+function inSession(){
+  var token = getCookie("access_token");
+  if(token == "None" || token == ""){
+    return false
+  } 
+  else {
+    return true
+  }
+}
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
 
 export default Main;
