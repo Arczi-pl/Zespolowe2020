7d1ea757a9447b222b01b8cecd5e64fc5bcbb7ee
phaser6 <73898927+phaser6@users.noreply.github.com>
2021-02-02 05:41:44 +0100

    Update About.js

--- React/src/components/About.js
+++ React/src/components/About.js
@@ -3,8 +3,13 @@ import React from 'react';
 const About = () => {
   return (
     <div className="limiter">
-    <div className="container-main">
-       Coming soon
+    <div className="container-about">
+       Projekt na kurs Programowanie Zespołowe na Politechnice Wrocławskiej
+
+Temat: Mini serwis hostingu plików wraz z możliwością<br/>
+prostego przesyłu i synchronizacji plików pomiędzy urządzeniami.<br/>
+
+Skład zespołu: Artur Sikorski, Jakub Sroka, Kamil Lachowski, Kacper Markowicz, Krzysztof Kądzioła, Piotr Piszczek
     </div>
   </div>
   );
