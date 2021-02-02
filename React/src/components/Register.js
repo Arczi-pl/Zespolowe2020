23c8381e13c679570b54827d29c40a80646e5557
Arczi-pl <56255448+Arczi-pl@users.noreply.github.com>
2021-02-02 01:04:38 +0100

    Rejestracja -> Logowanie -> Folder

--- React/src/components/Register.js
+++ React/src/components/Register.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Cookies from 'universal-cookie';
 import Tilt from 'react-tilt';
+import ReactDOM from 'react-dom';
+import { BiLogIn } from 'react-icons/bi';
 //Obrazy
 import send6 from './send6.png';
+import { MdCreateNewFolder } from 'react-icons/md';
 
 const Register = () => {
   return (
@@ -49,7 +53,7 @@ const Register = () => {
                   Zarejestruj
                 </button>
               </div>
-
+              <div id="error_msg"></div>
 
               <div className="text-center p-t-136">
                 <a className="txt2" href="#" onClick={goHomeSubmit}>
@@ -83,7 +87,7 @@ function goHomeSubmit(){
   window.location.href= path;
 }
 
-function registerSubmit(){
+function registerSubmit() {
 
   let payload = {"email":document.getElementById("email").value,
   "username":document.getElementById("username").value,
@@ -101,11 +105,108 @@ function registerSubmit(){
     }
 
     fetch(url, fetchOptions)
-    //TODO: Obsługa błedów (patrz Home.js)
+    .then(function(res){ 
+      return res.json(); })
+    .then(function(data){ 
+      var json = JSON.stringify(data)
+      var obj = JSON.parse(json)
 
-    let path = "/"; 
-    window.location.href= path;
+      if(obj.hasOwnProperty('detail')){
+        if(obj.detail== "Username already registered"){
+          ReactDOM.render(
+            <span style={{color: "red"}}>Użytkownik już zarejestrowany!</span>,
+            document.getElementById('error_msg'));
+        } else {
+          ReactDOM.render(
+            <span style={{color: "red"}}>Niepoprawny email!</span>,
+            document.getElementById('error_msg'))
+        }
+      }
+      else if(obj.hasOwnProperty("email")){
+        let payload = {email: document.getElementById("email").value,
+        password: document.getElementById("password").value}
+        login(payload)
+      }
+
+    });
+    
 
 }
 
+function login(payload){
+  let url = "/login";
+
+  let fetchOptions = {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload)
+    }
+
+    fetch(url, fetchOptions)
+    .then(function(res){ 
+      return res.json(); })
+    .then(function(data){ 
+      var json = JSON.stringify(data)
+      var obj = JSON.parse(json)
+      if(obj.hasOwnProperty('detail')){
+        ReactDOM.render(
+          <span style={{color: "red"}}>Nieprawidłowy login lub hasło</span>,
+          document.getElementById('error_msg')
+        );
+      }
+      else if(obj.hasOwnProperty("access_token")){
+        const cookies = new Cookies();
+        cookies.set("access_token", obj.access_token);
+        cookies.set("refresh_token", obj.refresh_token);
+        createNewFolderForUser();
+        let path = `/main`; 
+        window.location.href= path;
+      }
+
+
+    })
+}
+
+function createNewFolderForUser(){
+  let url = "/create_folder";
+
+  let payload = {folder_name: "main"}
+
+  let fetchOptions = {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      "Authorization": "Bearer " + getCookie("access_token")
+    },
+    body: JSON.stringify(payload)
+    }
+
+    fetch(url, fetchOptions)
+    .catch(function(err){
+      alert(err)
+    });
+}
+
+
+
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 export default Register;
